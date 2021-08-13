8aa51e72ee9926fe19b6a681f9d8ad7312a37c7f
sabarishkumarmca <sabarishkumarmca@gmail.com>
2021-08-13 17:15:52 +0530

    Role Permission Master Controller Added to the project

--- GCBS_INTERNAL/Controllers/API/Utilities/RolePermissionMastersController.cs
+++ GCBS_INTERNAL/Controllers/API/Utilities/RolePermissionMastersController.cs
@@ -1,5 +1,6 @@
 ﻿using GCBS_INTERNAL.Models;
 using GCBS_INTERNAL.Provider;
+using GCBS_INTERNAL.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -20,10 +21,74 @@ namespace GCBS_INTERNAL.Controllers.API.Utilities
         {
 
         }
-       
+        public MenuItemList GetRolePermissionMasters(int id)
+        {
+            return GetMenuItems(id);
+        }
+        
+
+        #region
+        private MenuItemList GetMenuItems(int RoleId)
+        {
+            MenuItemList menuItemList = new MenuItemList();
+            List<MenuItem> menuItem = new List<MenuItem>();
+            MenuItems menuItems = new MenuItems();
+            var list = menuItems.GetMenuItems();
+            var permissionViewModels = db.PermissionKeyValue.Select(x => new PermissionViewModel { Key = x.Key, Value = x.Value, Visible = x.Status }).ToList();
+            var permissionMaster = db.RolePermissionMaster.Where(x => x.RoleId == RoleId).ToList();
+            foreach (var a in list)
+            {
+                MenuItem menu = new MenuItem();
+                menu = a;
+                if (permissionMaster.Any(x => x.MenuId == a.MenuId))
+                {
+                    var b = permissionMaster.Where(x => x.MenuId == a.MenuId).FirstOrDefault();
+                    menu.Id = b.Id;
+                    menu.Status = PermissionConvert(permissionViewModels, b.Privilege);
+                }
+                else
+                {
+                    menu.Id = 0;
+                    menu.Status = PermissionConvert(permissionViewModels);
+                }
+                menuItem.Add(menu);
+            }
+            menuItemList.permissionViewModels = permissionViewModels;
+            menuItemList.menuItem = menuItem;
+            return menuItemList;
+        }
+        private List<bool> PermissionConvert(List<PermissionViewModel> permissionViews, string Permission = "")
+        {
+            List<bool> list = new List<bool>();
+            if (!String.IsNullOrEmpty(Permission))
+            {
+                var spl = Permission.Split('|');
+                foreach (var a in permissionViews)
+                {
+                    if (spl.Contains(a.Value))
+                    {
+                        list.Add(true);
+
+                    }
+                    else
+                    {
+                        list.Add(false);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var a in permissionViews)
+                {
+                    list.Add(false);
+                }
+            }
+            return list;
+        }
+        #endregion
 
         [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutRolePermissionMaster(RolePermissionRequest rolePermissionRequest)
+        public async Task<IHttpActionResult> PutRolePermissionMasters(RolePermissionRequest rolePermissionRequest)
         {
             if(rolePermissionRequest==null)
             {
