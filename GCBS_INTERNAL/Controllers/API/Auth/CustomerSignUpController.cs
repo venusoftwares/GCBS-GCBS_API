ffabb57fd0bb1cae7a67e452157111a681a03e10
VENU SOFTWARES <68750571+venusoftwares@users.noreply.github.com>
2021-10-07 16:04:45 +0530

    Bug fixing

--- GCBS_INTERNAL/Controllers/API/Auth/CustomerSignUpController.cs
+++ GCBS_INTERNAL/Controllers/API/Auth/CustomerSignUpController.cs
@@ -1,20 +1,28 @@
 ﻿using GCBS_INTERNAL.Models;
+using GCBS_INTERNAL.Services;
 using GCBS_INTERNAL.ViewModels;
+using IERP.Algorithum;
 using log4net;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 
 namespace GCBS_INTERNAL.Controllers.API.Auth
 {
     public class CustomerSignUpController : ApiController
     {
+        public string urlLink = ConfigurationManager.AppSettings["ApiUrl"];
         private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        private readonly DatabaseContext db = new DatabaseContext();
+        private readonly DatabaseContext db = new DatabaseContext(); 
+        private readonly SMTPService sMTPService = new SMTPService();
+        private readonly Algorithum algorithum = new Algorithum();
         public async Task<IHttpActionResult> PostCustomerSignUp(CustomerSignUp customerSignUp)
         {
             try
@@ -46,6 +54,19 @@ namespace GCBS_INTERNAL.Controllers.API.Auth
                 userManagement.Image = Constant.image;
                 db.UserManagement.Add(userManagement);
                 await db.SaveChangesAsync();
+                string path = HttpContext.Current.Server.MapPath("~/Template/EmailVerification.html");
+
+                string text = File.ReadAllText(path);
+
+                string userid = algorithum.Encrypt(userManagement.Id.ToString());  
+
+                string htmlString = text
+
+                    .Replace("{Email}", userManagement.EmailId)
+                    .Replace("{url}", $"{urlLink}/EmailVerification/token?code={userid}");
+
+                var res = sMTPService.Email(userManagement.EmailId, "Welcome to Golden Circle", htmlString);
+
                 return Content(HttpStatusCode.Created, "created successfully");
             }
             catch (Exception ex)
---
+++ GCBS_INTERNAL/Controllers/EmailVerification/EmailVerificationController.cs
@@ -0,0 +1,46 @@
+﻿using GCBS_INTERNAL.Models;
+using IERP.Algorithum;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GCBS_INTERNAL.Controllers.EmailVerification
+{
+    public class EmailVerificationController : Controller
+    {
+        public string FrontEndUrl = ConfigurationManager.AppSettings["FrontEndUrl"];
+
+        private readonly Algorithum algorithum = new Algorithum();
+
+        private DatabaseContext db = new DatabaseContext();
+
+        // GET: EmailVerification
+        [HttpGet]
+        public async Task<RedirectResult> token(string code)
+        {
+            if(!string.IsNullOrEmpty(code))
+            {
+                string userid = algorithum.Decrypt(code);
+
+                int userID = Convert.ToInt32(userid);
+
+                var userDetails =  db.UserManagement.Find(userID);
+
+                db.Entry(userDetails).State = EntityState.Modified;
+
+                await db.SaveChangesAsync();
+
+                return RedirectPermanent(FrontEndUrl);
+            }
+            else
+            {
+                return RedirectPermanent("https://www.google.com");
+            } 
+        }
+    }
+}
